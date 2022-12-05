bf7de62f22ebc00cf995d1db199875aed9b6f8be
lwfe <leonardolwfe@gmail.com>
2022-12-04 22:18:26 -0300

    Add: fetching pokeapi

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Container, Title } from "./styles";
 
 import { SearchBar } from "../../Components/SearchBar";
 
 export default function Home() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => response.json())
+      .then((data) => setPokemons(data.results))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Container>
       <StatusBar style="dark" />
