c14047106be3c935a90407fc968e8a7dd4486c2d
lwfe <leonardolwfe@gmail.com>
2023-05-16 05:16:07 -0300

    fix: pasta app fora da src

--- App.js
+++ App.js
@@ -3,15 +3,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFonts } from "expo-font";
 
-import Home from "./Screens/Home";
-import Details from "./Screens/Details";
+import Home from "./src/Screens/Home";
+import Details from "./src/Screens/Details";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [loaded] = useFonts({
-    "FiraCode-Regular": require("../assets/fonts/FiraCode-Regular.ttf"),
-    "FiraCode-SemiBold": require("../assets/fonts/FiraCode-SemiBold.ttf"),
+    "FiraCode-Regular": require("./assets/fonts/FiraCode-Regular.ttf"),
+    "FiraCode-SemiBold": require("./assets/fonts/FiraCode-SemiBold.ttf"),
   });
 
   if (!loaded) {
